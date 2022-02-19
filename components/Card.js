ba50ad9bf5cc125ff55b6adf05ea3cf362c2f028
Eren <forcex4545@gmail.com>
2022-02-19 22:10:04 +0000

    ♻️ Refactor

--- components/Card.js
+++ components/Card.js
@@ -2,19 +2,19 @@ function Card({ children, type }) {
     return (
         <>
             {type == "success" && (
-                <div className="w-auto h-auto p-3 text-black px-8">
+                <div className="rounded-xl	 w-auto h-auto p-3 text-black px-8">
                     {children}
                 </div>
             )}
 
             {type == "gray" && (
-                <div className="w-auto h-auto p-4 bg-[#444343] text-black px-8">
+                <div className="rounded-xl	w-auto h-auto p-10 bg-[#444343] text-black px-8">
                     {children}
                 </div>
             )}
 
             {!type && (
-                <div className="w-auto h-auto p-3 bg-white text-black px-8">
+                <div className="rounded-xl	w-auto h-auto p-3 bg-white text-black px-8">
                     {children}
                 </div>
             )}
--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import '../styles/globals.css'
 function Button({ children }) {
   return (
     <>
-      <span className="flex h-11 justify-center items-center p-2 border-2 border-[#b3aa11] rounded-2xl text-center w-28 cursor-pointer hover:bg-[#b3aa11]">
+      <span className="flex h-11 justify-center items-center p-2 border-2 border-[#fcee06] hover:duration-300 hover:text-black rounded-2xl text-center w-28 cursor-pointer hover:bg-[#fcee06]">
         {children}
       </span>
     </>
