7314c0d83d1104bc8f88dc1ed60785b1b3d19820
Eren <forcex4545@gmail.com>
2022-02-20 15:20:53 +0000

    feat: navigation bar set to responsive

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,8 @@
 import Header from "../components/layout/header";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
+
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
